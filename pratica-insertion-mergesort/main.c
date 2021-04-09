562eb500d982923d180c0fe24e18cb9cf090b64e
DoisDedin <joaovitorcotta@gmail.com>
2021-04-09 19:33:54 -0300

    erro not work but i go deliver wrong 2

--- pratica-insertion-mergesort/main.c
+++ pratica-insertion-mergesort/main.c
@@ -6,69 +6,60 @@
 int main() {
 
     ListaAluno *listaAluno = listaAluno_Inicia();
-    listaAluno_Insere(listaAluno, 0, 0, 1.0, 1.0);
-    listaAluno_Insere(listaAluno, 1, 1, 0.0, 0.0);
-    listaAluno_Insere(listaAluno, 2, 4, 7.0, 7.0);
-    listaAluno_Insere(listaAluno, 3, 2, 2.0, 2.0);
-    listaAluno_Insere(listaAluno, 4, 3, 5.5, 5.5);
-    listaAluno_Insere(listaAluno, 5, 3, 3.2, 1.5);
-    listaAluno_Insere(listaAluno, 6, 3, 5.5, 7.0);
-    listaAluno_Insere(listaAluno, 7, 3, 0.0, 9.0);
-   callMerge(listaAluno);
+     listaAluno_Imprime(listaAluno);
+        int decision;
+    printf("Escolha o metodo de ordenacao \n Insertion : 0 \nMerge : 1 \n");
+    scanf("%d", &decision);
+    if (decision == 0) {  //insertionSort
+        int size;
+        int sizeAlunos;
+        int matricula;
+        float notaProva;//0.7 valor
+        float notaTp;//0.3 valor
 
-    // listaAluno_Imprime(listaAluno);
-    //    int decision;
-//    printf("Escolha o metodo de ordenacao \n Insertion : 0 \nMerge : 1 \n");
-//    scanf("%d", &decision);
-//    if (decision == 0) {  //insertionSort
-//        int size;
-//        int sizeAlunos;
-//        int matricula;
-//        float notaProva;//0.7 valor
-//        float notaTp;//0.3 valor
-//
-//        scanf("%d", &size);
-//        int cont = 0;
-//        while (size != cont) {
-//            ListaAluno *listaAluno = listaAluno_Inicia();
-//            scanf("%d", &sizeAlunos);
-//            for (int i = 0; i < sizeAlunos; i++) {
-//                scanf("%d %f %f", &matricula, &notaProva, &notaTp);
-//                listaAluno_Insere(listaAluno, i, matricula, notaProva, notaTp);
-//            }
-//            printf("\n");
-//            // inserir o metodo de ordenação
-//          //  listaAluno_InsertionSort(listaAluno);
-//            listaAluno_Imprime(listaAluno);
-//            cont++;
-//        }
-//
-//
-//    } else { //mergeSort
-//        int size;
-//        int sizeAlunos;
-//        int matricula;
-//        float notaProva;//0.7 valor
-//        float notaTp;//0.3 valor
-//
-//        scanf("%d", &size);
-//        int cont = 0;
-//        while(size != cont){
-//            ListaAluno *listaAluno =listaAluno_Inicia();
-//            scanf("%d", &sizeAlunos);
-//            for(int i = 0 ; i < sizeAlunos; i++){
-//                scanf("%d %f %f", &matricula, &notaProva, &notaTp);
-//                listaAluno_Insere(listaAluno,i,matricula,notaProva,notaTp);
-//            }
-//            printf("\n");
-//
-//            //inserir o metodo de ordenação
-//
-//            listaAluno_Imprime(listaAluno);
-//            cont++;
-//        }
-//    }
-//
+        scanf("%d", &size);
+        int cont = 0;
+        while (size != cont) {
+            ListaAluno *listaAluno = listaAluno_Inicia();
+            scanf("%d", &sizeAlunos);
+            for (int i = 0; i < sizeAlunos; i++) {
+                scanf("%d %f %f", &matricula, &notaProva, &notaTp);
+                listaAluno_Insere(listaAluno, i, matricula, notaProva, notaTp);
+            }
+            printf("\n");
+            // inserir o metodo de ordenação
+            listaAluno_InsertionSort(listaAluno);
+          //  listaAluno_InsertionSort(listaAluno);
+           // listaAluno_Imprime(listaAluno);
+            cont++;
+        }
+
+
+    } else { //mergeSort
+        int size;
+        int sizeAlunos;
+        int matricula;
+        float notaProva;//0.7 valor
+        float notaTp;//0.3 valor
+
+        scanf("%d", &size);
+        int cont = 0;
+        while(size != cont){
+            ListaAluno *listaAluno =listaAluno_Inicia();
+            scanf("%d", &sizeAlunos);
+            for(int i = 0 ; i < sizeAlunos; i++){
+                scanf("%d %f %f", &matricula, &notaProva, &notaTp);
+                listaAluno_Insere(listaAluno,i,matricula,notaProva,notaTp);
+            }
+            printf("\n");
+
+            //inserir o metodo de ordenação
+            callMerge(listaAluno);
+            listaAluno_Imprime(listaAluno);
+            cont++;
+        }
+    }
+
 
     return 0;
 }
